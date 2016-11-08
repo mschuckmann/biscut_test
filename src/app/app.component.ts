e6797ce36dd9fd3baf0e11d8a7cb4c103263f387
Matt Schuckmann <matt@schuckmannacres.com>
2016-11-08 10:59:22 -0800

    Trying to add 2 videojs controls.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, OnDestroy, ViewChild } from '@angular/core';
+//import VideoJSPlayer from '@types/videojs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  private _elementRef: ElementRef;
+  private videoJSplayer1 : VideoJSPlayer;
+  private videoJSplayer2 : VideoJSPlayer;
+
+  //@ViewChild('video1') videoJSplayer1: VideoJSPlayer;
+  //@ViewChild('video2') videoJSplayer2: VideoJSPlayer;
+
+  constructor(elementRef: ElementRef) {
+    this._elementRef = elementRef
+  }
+
+  ngOnInit() {
+    console.log('Init - Component initialized')
+
+    this.videoJSplayer1 = videojs(document.getElementById('example_video_1'), {}, function() {
+        // This is functionally the same as the previous example.
+    });
+
+    this.videoJSplayer2 = videojs(document.getElementById('example_video_2'), {}, function() {
+        // This is functionally the same as the previous example.
+    });
+
+  }
+
+  ngOnDestroy() {
+    console.log('Deinit - Destroyed Component')
+    this.videoJSplayer1.dispose()
+    this.videoJSplayer2.dispose()
+  }
+
+  clicked() {
+    this.videoJSplayer1.play();
+    this.videoJSplayer2.play();
+  }
 }
